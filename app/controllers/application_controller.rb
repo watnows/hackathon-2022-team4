1e514cec8e2fe892b0faa7bc3fec0a024feb6004
Nakagawa Shinichi <98950032+shinpochi2002@users.noreply.github.com>
2022-08-06 19:04:13 +0900

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  # ログイン後に遷移するpathを設定
+  def after_sign_in_path_for(resource)
+    new_user_password_path
+  end
+
+  def after_sign_up_path_for(resource)
+    new_user_password_path
+  end
+ 
+  # ログアウト後に遷移するpathを設定
+  def after_sign_out_path_for(resource)
+    tool_index_path
+  end
 end
