59505489e5c2edd671ac9ecaf5f85614df658709
TAKAYAMAN <sirokurodog_2180@yahoo.co.jp>
2022-08-08 08:17:10 +0900

    showとindexの実装

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,3 +4,5 @@
 //= link_tree ../../../vendor/javascript .js
 //= link login/login_style.css
 //= link post/post_style.css
+//= link index/index_style.css
+//= link index/show_style.css
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.precompile+=%w(login/*)
 Rails.application.config.assets.precompile+=%w(post/*)
+Rails.application.config.assets.precompile+=%w(index/*)
 
 
 # Add additional assets to the asset load path.
